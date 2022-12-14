c3beebe8202755d3a0bd2de8d1f27a8cc7804fc6
Bhavika Choudhary <69667567+bhavikac16@users.noreply.github.com>
2022-12-15 04:20:14 +0530

    Add files via upload

---
+++ gesture py code.py
@@ -0,0 +1,31 @@
+import serial #for Serial communication
+import time 
+import pyautogui
+ 
+ArduinoSerial = serial.Serial('com6',9600) 
+time.sleep(2) #wait for 2 seconds for the communication to get established
+
+while 1:
+    incoming = str(ArduinoSerial.readline()) #read the serial data and print it as line
+    print(incoming)
+      
+    
+    if 'Play/Pause' in incoming:
+        pyautogui.typewrite(['space'], 0.2)
+
+    if 'Rewind' in incoming:
+        pyautogui.hotkey('ctrl', 'left')  
+
+    if 'Forward' in incoming:
+        pyautogui.hotkey('ctrl', 'right') 
+
+    if 'Vup' in incoming:
+        pyautogui.hotkey('ctrl', 'down')
+        
+    if 'Vdown' in incoming:
+        pyautogui.hotkey('ctrl', 'up')
+
+    if 'next' in incoming:
+        pyautogui.typewrite('n',0.2)
+
+    incoming = "";
